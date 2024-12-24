a588f699116fe9def75b4744a0d10e27031db458
Spuke337 <alahababaha@gmail.com>
2024-12-24 17:11:40 +0400

    сохраняю

---
+++ script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+(function(d,s,id,u){
+  if (d.getElementById(id)) return;
+  var js, sjs = d.getElementsByTagName(s)[0],
+      t = Math.floor(new Date().getTime() / 1000000);
+  js=d.createElement(s); js.id=id; js.async=1; js.src=u+'?'+t;
+  sjs.parentNode.insertBefore(js, sjs);
+}(document, 'script', 'os-widget-jssdk', 'https://www.opinionstage.com/assets/loader.js'));
+</script><div id="os-widget-1310514" class="os_widget" data-path="/spuke228/39ff6ec6-eba3-4327-87ff-35f32b67f4bd" data-of="spuke228" data-opinionstage-widget="39ff6ec6-eba3-4327-87ff-35f32b67f4bd"></div>
